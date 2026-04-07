778095c52fb83fdcc448c8fb53e88402771a9ad9
Manan Shah <163020288+Technova100@users.noreply.github.com>
2026-04-08 02:19:45 +0530

    Delete Taha bha/InfoAI-main directory

--- Taha bha/InfoAI-main/backend/compiler/__init__.py
+++
@@ -1,4 +0,0 @@
-# Compiler module for Visual Blueprint to Nano Banana Pro prompt conversion
-from .prompt_compiler import compile_prompt, validate_blueprint
-
-__all__ = ['compile_prompt', 'validate_blueprint']
